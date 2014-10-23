f2e691b994b533f3feaa5319036a0d614bcac5f8
Marion Le Borgne <leborgne.mar@gmail.com>
2014-10-23 10:36:48 -0700

    Update BalanceTreeChecker.java

--- BalanceTreeChecker.java
+++ BalanceTreeChecker.java
@@ -1,6 +1,9 @@
 /**
 
-Implement a function to check if a binary tree is balanced. For the purpose of this question,  a balanced tree is desfined to be a tree such that the heights of the two subtree of any node never differ by more than one.
+Implement a function to check if a binary tree is balanced. 
+For the purpose of this question,  a balanced tree is defined
+to be a tree such that the heights of the two subtree of 
+any node never differ by more than one.
 
 **/
 
